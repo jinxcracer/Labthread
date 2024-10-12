f746a491f47d0b05594a1f9e02bf047df59d6516
jinxcracer <113971845+jinxcracer@users.noreply.github.com>
2024-10-12 05:19:53 -0700

    Add files via upload

---
+++ FrmBasicThread/FrmBasicThread/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace FrmBasicThread
+{
+    public partial class Form1 : Form
+    {
+        private Thread ThreadA;
+        private Thread ThreadB;
+
+        public Form1()
+        {
+            InitializeComponent();
+            lblStatus.Text = "-Before Starting Thread-";  
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ThreadA = new Thread(MyThreadClass.Thread1) { Name = "Thread A" };
+            ThreadB = new Thread(MyThreadClass.Thread1) { Name = "Thread B" };
+
+            
+            ThreadA.Start();
+            ThreadB.Start();
+
+            
+            ThreadA.Join();
+            ThreadB.Join();
+
+            
+            lblStatus.Text = "-End of Thread-";
+        }
+    }
+}
---
+++ FrmBasicThread/FrmBasicThread/MyThreadClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+public class MyThreadClass
+{
+    public static void Thread1()
+    {
+        for (int loopCount = 0; loopCount <= 5; loopCount++) 
+        {
+            Thread thread = Thread.CurrentThread;  
+            Console.WriteLine("Name of Thread: " + thread.Name + " = " + loopCount);
+
+            
+            Thread.Sleep(1500);
+        }
+    }
+}
